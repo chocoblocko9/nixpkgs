b0494301e6b10fbe1e3b63f2e18ad6a654b9c643
Conor <conorboyle07@protonmail.com>
2026-06-17 01:03:27 +0100

    firefox: fix-build on musl also adds vine cus oops

---
+++ pkgs/by-name/vi/vine/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "vine";
+  version = "0-unstable-2026-07-04";
+
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "VineLang";
+    repo = "vine";
+    rev = "6620059fe341285d6a22b58ce4d3d9dc1b1e9036";
+    hash = "sha256-sWzJflnDNXN/Cddj/J8AOVy1Lu7w1We1k+KyJf1FoLw=";
+  };
+
+  cargoHash = "sha256-RHAC09589XQuewvSZ5UAaM1i0/fF9S5vTAoD6/cbGNc=";
+
+  # cargoExtraArgs = "--bin vine";
+  env.VINE_ROOT_PATH = "../lib/root";
+
+  postInstall = ''
+    mkdir -p $out/lib
+    cp -r root $out/lib/root
+  '';
+
+  meta = {
+    description = "Experimental new programming language based on interaction nets";
+    homepage = "https://github.com/VineLang/vine";
+    # changelog = "https://github.com/ownself/wewa/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ choco98 ];
+    mainProgram = "vine";
+  };
+})
+
